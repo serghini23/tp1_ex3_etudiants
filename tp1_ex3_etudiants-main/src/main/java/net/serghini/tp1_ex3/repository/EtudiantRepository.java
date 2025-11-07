69c8266714775c2fe3a84f492abb6ae0e4ab359f
serghini23 <serghinhoakram@gmail.com>
2025-11-07 23:50:36 +0100

    v3 fix issues duplicate files

--- tp1_ex3_etudiants-main/src/main/java/net/serghini/tp1_ex3/repository/EtudiantRepository.java
+++
@@ -1,7 +0,0 @@
-package net.serghini.tp1_ex3.repository;
-
-import net.serghini.tp1_ex3.entities.Etudiant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
-}
